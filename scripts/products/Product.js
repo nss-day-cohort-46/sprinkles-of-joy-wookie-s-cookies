acaea02babc5b9a10187a9fbe38b563be5fc4b58
Nick Carver <nicholascarver2@outlook.com>
2021-02-19 15:44:28 -0600

    the products now are successfully added and render to the cart

--- scripts/products/Product.js
+++ scripts/products/Product.js
@@ -8,7 +8,7 @@ export const Product = (product, category) => {
               <p>$${product.price}</p>
           </header>
           <div>
-              <button id="addProduct">Add to Cart</button>
+              <button id="addProduct--${product.id}">Add to Cart</button>
               <p>${product.description} [${category.name}]</p>
           </div>
       </section>
