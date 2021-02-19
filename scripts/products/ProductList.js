df539512b4ffab39524a4b704d220fb3e1471ec0
Nick Carver <nicholascarver2@outlook.com>
2021-02-19 11:34:19 -0600

    added comments

--- scripts/products/ProductList.js
+++ scripts/products/ProductList.js
@@ -18,17 +18,24 @@ export const ProductList = () => {
     })
 }
 
+//takes an array of bakery products
 const render = (arrayOfBakeryProducts) => {
+  //maps over each bakery product to find the category associated with that product
   contentTarget.innerHTML = arrayOfBakeryProducts.map(product => {
     const productCategory = bakeryCategories.find(category => category.id === product.categoryId)
+    //returns HTML of each product with info about it and the category it is associated with
     return Product(product, productCategory)
   }).join("")
 }
 
+
+//Listens for a selection on the category dropdown
 eventHub.addEventListener("categorySelected", Event => {
+  //Runs when certain category is selected. Filters all of the bakery products down to the ones that have the corresponding categoryId to the selection.
   if (Event.detail.selectedCategory !== 0) {
     const allBakeryProducts = useProducts()
     const filteredProducts = allBakeryProducts.filter(product => product.categoryId === Event.detail.selectedCategory)
+    //sends the filted products to get rendered to ProductList.
     render(filteredProducts)
   }
 })
