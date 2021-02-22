956aff14d05fc54474709b51bfe43a833e593c0b
Nick Carver <nicholascarver2@outlook.com>
2021-02-22 11:31:09 -0600

    added a roundoff on rating

--- scripts/products/Product.js
+++ scripts/products/Product.js
@@ -17,7 +17,7 @@ export const Product = (productObj, categoryObj, reviewArray) => {
           </header>
           <div>
               <button id="addProduct--${productObj.id}">Add to Cart</button>
-              <p id="review--${productObj.id}">Rating: ${averageRating} out of 5</p>
+              <p id="review--${productObj.id}">Rating: ${averageRating.toFixed(1)} out of 5</p>
               <a href="#">see all reviews</a>
               <p>${productObj.description} [${categoryObj.name}]</p>
           </div>
