e333b06547db6d4eb014d8acf5ab04e2bfa6ef38
kylieanyce <kylieanyce@gmail.com>
2021-02-22 11:57:58 -0600

    removed a debugger

--- scripts/contact/ContactProvider.js
+++ scripts/contact/ContactProvider.js
@@ -2,7 +2,6 @@ import { bakeryAPI } from "../Settings.js"
 
 //saves contact info to API---------------------------------------
 export const saveContact = (contacts) => {
-    debugger
     return fetch(`${bakeryAPI.baseURL}/contact`, {
         method: "POST",
         headers: {
