ac850489c99f3e1677419bc251d4dfe8c3a4ddad
kylieanyce <kylieanyce@gmail.com>
2021-02-22 15:30:07 -0600

    delete button renders to DOM for only orders that are ready for pick up

--- scripts/orders/Order.js
+++ scripts/orders/Order.js
@@ -1,9 +1,15 @@
 export const Order = (customerOrder) => {
+  let deleteButton = ""
+  if (customerOrder.status.id === 1){
+    deleteButton = `<p><button id="deleteOrder">Delete Order</button><p>`
+  } 
   return `
     <div class="order" id="${customerOrder.id}">
       <p>${new Date(customerOrder.timestamp).toLocaleString('en-US')}</p>
       <p>${customerOrder.status.label}</p>
       <p>Order Number: ${customerOrder.id}</p>
-    </div>
+      </div>
+      ${deleteButton}
   `
 }
+
