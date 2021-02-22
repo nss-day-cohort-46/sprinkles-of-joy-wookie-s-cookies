1145586d68f9c11b986e90307d9bc9686ca15286
Nick Carver <nicholascarver2@outlook.com>
2021-02-22 17:03:08 -0600

    added the module in main. Removed a console log from LoginForm. Trying to figure out the timeout function.

--- scripts/reviews/ReviewSumbittedMessage.js
+++ scripts/reviews/ReviewSumbittedMessage.js
@@ -1,23 +1,23 @@
 const eventHub = document.querySelector("#container")
 const contentContainer = document.querySelector(".disappearingMessages")
 
-const ReviewSubmitted = lastCustomerOrder => {
+const ReviewSubmitted = () => {
     render()
 }
 
 const render = () => {
     contentContainer.innerHTML = `
-  <div id="reviewSubmittedMessage__modal" class="modal--parent">
+    <div id="reviewSubmittedMessage__modal" class="modal--parent">
         <div class="modal--content">
         <h3>Your Review Has Been Submitted</h3>
         </div>
     </div>
-      `
+    `
 }
 
 eventHub.addEventListener("newReviewAdded", event => {
     ReviewSubmitted()
-    setTimeout(clsoeModal(), 2000)
+    setTimeout(closeModal(), 2000)
 })
 
 //clears modal HTML
