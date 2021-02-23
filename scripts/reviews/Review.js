32f8752369c14759c76961a5a3335f920d4f3083
Nick Carver <nicholascarver2@outlook.com>
2021-02-23 14:23:20 -0600

    added a delete button to reviews written by a logged in user

--- scripts/reviews/Review.js
+++ scripts/reviews/Review.js
@@ -1,9 +1,19 @@
+import { authHelper } from "../auth/authHelper.js"
+
 export const Review = (review, customer) => {
+    let button = ""
+    if (authHelper.isUserLoggedIn()) {
+        let customerId = parseInt(authHelper.getCurrentUserId())
+        if (customerId === customer.id) {
+            button = `<button id="deleteReviewButton">Delete</button>`
+        }
+    }
     return `
         <article class="productReview">
             <h3>${review.rating} out of 5 stars</h3>
             <p>"${review.reviewText}"</p>
             <p>-${customer.name}</p>
+            ${button}
         </article>
 `
 }
